62a8cd865003df21c2d0c9eba0a5f27e85634966
Venkat Malladi <vmalladi@microsoft.com>
2023-03-03 15:54:18 +0000

    Work on all stats at once.

--- misc/collect_runtimes.py
+++ misc/collect_runtimes.py
@@ -12,9 +12,9 @@ import sys
 
 def parse_args(argv=None):
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("--input_directory", nargs="+", help="A directory containing benchmark results")
-    parser.add_argument("--instance_price", nargs="+", help="The instance price in $/hr")
-    parser.add_argument("--instance_type", nargs="+", help="The instance type")
+    parser.add_argument("--input_directory", help="A directory containing benchmark results")
+    parser.add_argument("--instance_price", type=float, help="The instance price in $/hr")
+    parser.add_argument("--instance_type", help="The instance type")
     parser.add_argument("--outfile", default=sys.stdout, type=argparse.FileType('w'))
     return parser.parse_args(argv)
 
@@ -26,25 +26,70 @@ def bm_file_to_sec(bm_path):
     return float(values[0])
 
 def main(args):
-    assert len(args.input_directory) == len(args.instance_price)
-    assert len(args.input_directory) == len(args.instance_type)
 
-    # Iterate over each instance type
+    targets=[
+        "DNAseq/Illumina_HG002_HiSeqX_40x",
+        "DNAseq/Illumina_HG002_NovaSeq_40x",
+        "DNAscope/Illumina_HG002_HiSeqX_40x",
+        "DNAscope/Illumina_HG002_NovaSeq_40x",
+        "DNAscope/Element_HG002_100x",
+        "DNAscope/Ultima_HG002_cram",
+        "DNAscope_LongRead/PacBio_HG002_HiFi_Chem2",
+    ]
+
+
+    # Iterate over each target
     overall_results = collections.OrderedDict()
-    for in_dir, price, instance_type in zip(args.input_directory, args.instance_price, args.instance_type):
+    in_dir = str(args.input_directory)
+    price = args.instance_price
+    instance_type = args.instance_type
+    for target in targets:
         runtime_in_sec = collections.OrderedDict()
         price = float(price)
 
-        # Alignment runtimes
-        alignment_results = glob.glob(os.path.join(in_dir, "ramdisk/bwa_mem/Illumina_HG002_NovaSeq_40x/0/*/*/alignments.bam.benchmark.txt"))
-        runtime_in_sec["Alignment"] = max([bm_file_to_sec(x) for x in alignment_results])  # One or more alignemnt jobs run in parallel. All jobs need to finish before the next step, so total runtime is the max of all runtimes
+        target_split = target.split('/')
+        pipeline = target_split[0] 
+        sample = target_split[1]
+        if (pipeline == "DNAseq"):
 
-        # Other stages
-        runtime_in_sec["LocusCollector"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/lc/Illumina_HG002_NovaSeq_40x/score.txt.gz.benchmark.txt"))
-        runtime_in_sec["Dedup"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/dedup/Illumina_HG002_NovaSeq_40x/dedup.bam.benchmark.txt"))
-        runtime_in_sec["QualCal"] = bm_file_to_sec(os.path.join(in_dir, "qualcal/Illumina_HG002_NovaSeq_40x/recal.table.benchmark.txt"))
-        runtime_in_sec["Haplotyper"] = bm_file_to_sec(os.path.join(in_dir, "haplotyper/Illumina_HG002_NovaSeq_40x/calls.vcf.gz.benchmark.txt"))
+            # Alignment runtimes
+            alignment_results = glob.glob(os.path.join(in_dir, "ramdisk/bwa_mem", sample,  "0/*/*/alignments.bam.benchmark.txt"))
+            runtime_in_sec["Alignment"] = max([bm_file_to_sec(x) for x in alignment_results])  # One or more alignemnt jobs run in parallel. All jobs need to finish before the next step, so total runtime is the max of all runtimes
 
+            # Preprocessing
+            runtime_in_sec["LocusCollector"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/lc", sample, "score.txt.gz.benchmark.txt"))
+            runtime_in_sec["Dedup"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/dedup", sample, "dedup.bam.benchmark.txt"))
+            runtime_in_sec["QualCal"] = bm_file_to_sec(os.path.join(in_dir, "qualcal", sample, "recal.table.benchmark.txt"))
+            
+            # Variant Calling
+            runtime_in_sec["Haplotyper"] = bm_file_to_sec(os.path.join(in_dir, "haplotyper", sample, "calls.vcf.gz.benchmark.txt"))
+        
+        elif (pipeline == 'DNAscope'):
+            if (sample != 'Ultima_HG002_cram'):
+                # Alignment runtimes
+                alignment_results = glob.glob(os.path.join(in_dir, "ramdisk/bwa_mem", sample,  "0/*/*/alignments.bam.benchmark.txt"))
+                runtime_in_sec["Alignment"] = max([bm_file_to_sec(x) for x in alignment_results])  # One or more alignemnt jobs run in parallel. All jobs need to finish before the next step, so total runtime is the max of all runtimes
+            
+
+                # Preprocessing
+                runtime_in_sec["LocusCollector"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/lc", sample, "score.txt.gz.benchmark.txt"))
+                runtime_in_sec["Dedup"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/dedup/dedup", sample, "dedup.bam.benchmark.txt"))
+            
+            # Variant Calling
+            runtime_in_sec["Dnascope"] = bm_file_to_sec(os.path.join(in_dir, "dnascope", sample, "calls.vcf.gz.benchmark.txt"))
+            runtime_in_sec["Dnascope_tmp"] = bm_file_to_sec(os.path.join(in_dir, "dnascope_tmp", sample, "calls.vcf.gz.benchmark.txt"))
+        
+        elif (pipeline == 'DNAscope_LongRead'):
+            # Alignment runtimes
+            alignment_results = glob.glob(os.path.join(in_dir, "ramdisk/minimap2", sample, "*/alignments.bam.benchmark.txt"))
+            runtime_in_sec["Alignment"] = sum([bm_file_to_sec(x) for x in alignment_results])  # One or more alignemnt jobs run in parallel. All jobs need to finish before the next step, so total runtime is the max of all runtimes
+
+            # Preprocssing
+            runtime_in_sec["MergePb"] = bm_file_to_sec(os.path.join(in_dir, "ramdisk/merge_pb", sample, "merged.bam.benchmark.txt"))
+            
+            # Variant Calling
+            runtime_in_sec["Dnascope"] = bm_file_to_sec(os.path.join(in_dir, "dnascope_lr", sample, "calls.vcf.gz.benchmark.txt"))
+        
         # summary results
         results = collections.OrderedDict()
         results["Total (s)"] = sum(runtime_in_sec.values())
@@ -54,15 +99,20 @@ def main(args):
 
         # Collect everything together
         runtime_in_sec.update(results)
-        overall_results[instance_type] = runtime_in_sec.copy()
+        overall_results[target] = runtime_in_sec.copy()
+        
+
 
     # Output a nice table
-    row = ["Instance type"] + args.instance_type
-    print('\t'.join(row), file=args.outfile)
-    row_names = list(overall_results[args.instance_type[0]].keys())
-    for row_name in row_names:
-        row = [row_name] + [str(overall_results[x][row_name]) for x in args.instance_type]
-        print('\t'.join(row), file=args.outfile)
+    print(args.instance_type, file=args.outfile)
+    print("\n", file=args.outfile)
+    for sample in targets:
+        row_names = list(overall_results[sample].keys())
+        print(sample, file=args.outfile)
+        for row_name in row_names:
+            row = [row_name] + [str(overall_results[sample][row_name])]
+            print('\t'.join(row), file=args.outfile)
+        print("\n", file=args.outfile)
 
     return 0
 
