30a8a55fb42abea1cadb77dac79501ba9e106b86
Venkat Malladi <vmalladi@microsoft.com>
2024-09-10 15:49:38 -0500

    Update instance setup for arm.

--- misc/instance_setup.sh
+++ misc/instance_setup.sh
@@ -16,7 +16,7 @@ sudo apt update -y
 sudo apt install -y
 sudo apt install -y autoconf automake make gcc libdata-dump-perl zlib1g zlib1g-dev  \
     bzip2 libbz2-dev xz-utils curl openssl libncurses5-dev libncursesw5-dev libtool \
-    nasm yasm gzip git unzip hostname cmake make gcc g++ autoconf liblzma-dev python2
+    nasm yasm gzip git unzip hostname cmake make gcc g++ autoconf liblzma-dev
 
 # Get the software packge
 cd "$INSTALL_DIR"
@@ -75,8 +75,21 @@ make
 cd "$INSTALL_DIR"
 mkdir -p bedtools/2.30.0
 cd bedtools/2.30.0
-curl -L -o "bedtools" "https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools.static.binary"
-chmod ugo+x ./bedtools
+if [[ "$architechure" == "x86_64" ]]; then
+    curl -L -o "bedtools" "https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools.static.binary"
+    chmod ugo+x ./bedtools
+elif [[ "$architechure" == "arm" || "$architechure" == "aarch64" ]]; then
+     wget https://github.com/arq5x/bedtools2/releases/download/v2.30.0/bedtools-2.30.0.tar.gz
+     tar -zxvf bedtools-2.30.0.tar.gz
+     cd bedtools2
+     sed -i '/^#include <cstdlib>/s/^/#include <cstdint>\n/' src/utils/general/ParseTools.h
+     make
+     mv bin/bedtools ../2.30.0/
+     
+else
+    echo "UNKNOWN ARCHITECHURE"
+    exit 1
+fi
 
 # install samtools
 cd "$INSTALL_DIR"
@@ -100,7 +113,7 @@ cd bcftools-1.15.1
 cd "$INSTALL_DIR"
 mkdir -p rtgtools
 cd rtgtools
-curl -L -O "https://github.com/RealTimeGenomics/rtg-tools/releases/download/3.12.1/rtg-tools-3.12.1-linux-x64.zip"
+curl -L -O "https://github.com/RealTimeGenomics/rtg-tools/releases/download/3.12.1/rtg-tools-3.12.1-nojre.zip"
 unzip rtg-tools-3.12.1-linux-x64.zip
 ## Configure rtgtools
 rtg_config="rtg-tools-3.12.1/rtg.cfg"
@@ -108,44 +121,53 @@ echo "RTG_TALKBACK=" > "$rtg_config"
 echo "RTG_USAGE=" >> "$rtg_config"
 echo "RTG_USAGE_OPTIONAL=username,hostname" >> "$rtg_config"
 
-# install happy
-cd "$INSTALL_DIR"
-mkdir -p happy
-cd happy
-curl -L "https://github.com/Illumina/hap.py/archive/refs/tags/v0.3.15.tar.gz" | \
-    tar -zxf -
-cd hap.py-0.3.15
-python2 ./install.py "$INSTALL_DIR"/happy/hap.py-install --no-tests
-
 # Install snakemake with pip
 cd "$INSTALL_DIR"
-curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b
+if [[ "$architechure" == "x86_64" ]]; then
+    curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash Miniconda3-latest-Linux-x86_64.sh -b
+elif [[ "$architechure" == "arm" || "$architechure" == "aarch64" ]]; then
+    curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+    bash Miniconda3-latest-Linux-aarch64.sh -b
+else
+    echo "UNKNOWN ARCHITECHURE"
+    exit 1
+fi
+
 python3=/home/azureuser/miniconda3/bin/python
 pip3=/home/azureuser/miniconda3/bin/pip
 $pip3 install snakemake pandas
 snakemake=/home/azureuser/miniconda3/bin/snakemake
 
 # Install the sentieon-cli
+$pip3 install poetry
+poetry=/home/azureuser/miniconda3/bin/poetry
 cd "$INSTALL_DIR"
-curl -LO https://github.com/sentieon/sentieon-cli/releases/download/v1.0.0/sentieon_cli-1.0.0.tar.gz
-$pip3 install sentieon_cli-1.0.0.tar.gz
+git clone https://github.com/Sentieon/sentieon-cli.git
+cd sentieon-cli
+$poetry config virtualenvs.in-project true
+$poetry install
+
+# install minimap2 for arm
+curl -L "https://github.com/lh3/minimap2/archive/refs/tags/v2.26.tar.gz" | \
+    tar -zxf -
+cd minimap2-2.26
+make arm_neon=1 aarch64=1
 
 # install miniconda2 for hap.py
 cd "$INSTALL_DIR"
-curl -LO https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh -b
-python2=/home/azureuser/miniconda2/bin/python
-pip2=/home/azureuser/miniconda2/bin/pip
-
-# arm
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-sudo python2 get-pip.py
-python2=/usr/bin/python2
-pip2=/usr/local/bin/pip2
-
-
-
-# install hap.py dependencies into the py2 env
-pip2 install pysam pandas scipy
-
+if [[ "$architechure" == "x86_64" ]]; then
+    curl -LO https://repo.anaconda.com/miniconda/Miniconda2-latest-Linux-x86_64.sh
+    bash Miniconda2-latest-Linux-x86_64.sh -b
+    python2=/home/azureuser/miniconda2/bin/python
+    pip2=/home/azureuser/miniconda2/bin/pip
+    pip2 install pysam pandas scipy
+    # install happy
+    cd "$INSTALL_DIR"
+    mkdir -p happy
+    cd happy
+    curl -L "https://github.com/Illumina/hap.py/archive/refs/tags/v0.3.15.tar.gz" | \
+        tar -zxf -
+    cd hap.py-0.3.15
+    python2 ./install.py "$INSTALL_DIR"/happy/hap.py-install --no-tests
+fi
